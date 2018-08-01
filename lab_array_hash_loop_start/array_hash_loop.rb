79e509d06328762508de279099a96871201714b6
Laura Nagy <ngylaura@gmail.com>
2018-08-01 15:45:51 +0100

    first commit of Lab Day 3: Jesse & Laura

---
+++ lab_array_hash_loop_start/array_hash_loop.rb
@@ -0,0 +1,26 @@
+def add_array_lengths( array1, array2 )
+  return array1.length() + array2.length()
+end
+
+
+def sum_array(numbers)
+  sum = 0
+  for number in numbers
+    sum += number
+  end
+  return sum
+end
+
+def find_item(array, item)
+  for elem in array
+    if (elem == item)
+      return true
+    end
+  end
+  return false
+end
+
+def get_first_key(hash)
+  arr = hash.keys()
+  return arr[0]
+end
